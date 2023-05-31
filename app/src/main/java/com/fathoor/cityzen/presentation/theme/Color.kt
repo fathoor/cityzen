0569c5456c94da512ceeb065805eba7d40d7c5f2
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-31 19:59:36 +0700

    `add` main activity

---
+++ app/src/main/java/com/fathoor/cityzen/presentation/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.fathoor.cityzen.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CityzenBlue = Color(0xFF6CABDD)
+val CityzenNavy = Color(0xFF1C2C5B)
+val CityzenGold = Color(0xFFFFC659)
